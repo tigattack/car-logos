f20ec6b8d83d848aed381fa98452474f02e4b398
S33G <dev@charlie.fyi>
2024-09-12 02:33:39 +0100

    sort of works

---
+++ ui/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "../components/Search";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Vehicle Logo DB</h1>
+      <Search />
+    </div>
+  );
+};
+
+export default Home;

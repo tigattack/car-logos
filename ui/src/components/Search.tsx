9487305d4965ed07984e0cf5652bd7b7300c3deb
seeg <seeg@dev.dev.local>
2024-09-16 01:34:59 +0100

    chore(Search): Add some error handling

--- ui/src/components/Search.tsx
+++ ui/src/components/Search.tsx
@@ -5,49 +5,61 @@ import React, { useState, useEffect } from 'react';
 import SearchItem from './SearchItem';
 
 interface Entity {
-    id: number;
-    name: string;
-    url: string;
+  id: number;
+  name: string;
+  url: string;
 }
 
 const Search: React.FC = () => {
-    const [query, setQuery] = useState('');
-    const [results, setResults] = useState<Entity[]>([]);
+  const [query, setQuery] = useState('');
+  const [results, setResults] = useState<Entity[]>([]);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch('/logos.json')
-        .then((response) => response.json())
-        .then((data) => {
-            setResults(data);
+  useEffect(() => {
+    fetch('/logos.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setResults(data);
+        setError(data.error);
 
-            // TODO: Fix this.
-            // It's annoying we dont have a server
-            // if (!query) return
-            // const filteredLogos = results.filter((entity: Entity) => {
-            //     console.log(entity);
-            // });
-        });
+        // TODO: Fix this.
+        // It's annoying we dont have a server
+        // if (!query) return
+        // const filteredLogos = results.filter((entity: Entity) => {
+        //     console.log(entity);
+        // });
+      }).catch((error) => {
+        console.error('Error:', error);
+      });
 
-        return () => {
-            setResults(results ?? []);
-        }
-    }, [query]);
+    return () => {
+      setResults(results ?? []);
+    }
+  }, [query]);
 
-    return (
-        <div>
-            {/* <input
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!results) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      {/* <input
                 type="text"
                 placeholder="Search logos..."
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
             /> */}
-            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))', gap: '.5rem' }}>
-                {results.map((logo) => (
-                    <SearchItem key={logo.id} logoUrl={'/' + logo.image.path} label={logo.name} />
-                ))}
-            </div>
-        </div>
-    );
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))', gap: '.5rem' }}>
+        {results.map((logo) => (
+          <SearchItem key={logo.id} logoUrl={'/' + logo.image.path} label={logo.name} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Search;

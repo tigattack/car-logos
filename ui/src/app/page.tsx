34c669364e71d27487e0f8cb91ea9df4c024b9cd
seeg <seeg@dev.dev.local>
2024-09-16 01:35:42 +0100

    chore(layout): Remove title

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -4,7 +4,6 @@ import React, { use } from 'react';
 const HomePage: React.FC = () => {
     return (
         <div>
-            <h1>Welcome to Car Logos</h1>
             <Search />
         </div>
     );
